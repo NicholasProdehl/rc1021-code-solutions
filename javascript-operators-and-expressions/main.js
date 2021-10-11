13273ae7e0fda9169c6efe9df973790c8a363535
Nicholas Prodehl <nicholasprodehl@gmail.com>
2021-10-11 14:57:48 -0500

    Finished it yay.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 40;
+var height = 50;
+var area = width * height;
+console.log('Value of area is: ', area);
+console.log('Type of area is: ', typeof area);
+
+var bill = 100;
+var payment = 200;
+var change = payment - bill;
+console.log('Value of change is: ', change);
+console.log('Type of change is: ', typeof change);
+
+var quizzes = 4;
+var midterm = 6;
+var final = 8;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of grade is: ', grade);
+console.log('Type of grade is: ', typeof grade);
+
+var firstName = 'Nicholas';
+var lastName = 'Prodehl';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName is: ', fullName);
+console.log('Type of fullName is: ', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+console.log('Value of isAcidic is: ', isAcidic);
+console.log('Type of isAcidic is: ', typeof isAcidic);
+
+var headCount = 666;
+var isSparta = headCount === 300;
+console.log('Value of isSparta is: ', isSparta);
+console.log('Type of isSparta is: ', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT.';
+console.log('Value of motto is: ', motto);
+console.log('Type of motto is: ', typeof motto);
