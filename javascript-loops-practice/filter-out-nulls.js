a650008707245d3025717b70e9d79ee26a3a797f
Nicholas Prodehl <nicholasprodehl@gmail.com>
2021-10-15 12:15:58 -0500

    Finished the PseudoCode

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -8,3 +8,14 @@ function filterOutNulls(values) {
   }
   return counter;
 }
+
+// declare a function filterOutNulls that expects a parameter of values
+// declare a variable counter with an empty array
+// declare a for loop, intializing the variable i with value 0,
+// setting a conditional expression i < values.length
+// incrementing i++. The idea is to get the for loop to run the same amount of times
+// as the length of the values array.
+// every time running through the loop, check for the current array positions value.
+// check if it's strictly not equal to the value null, if it is not, push that value into
+// the counter array we declared earlier.
+// then return that array from the function.
