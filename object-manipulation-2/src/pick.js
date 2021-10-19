f37b0907b8a9cf85dc2753c681daeddc96c9f61f
Nicholas Prodehl <nicholasprodehl@gmail.com>
2021-10-19 12:18:43 -0500

    Not quite done.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,10 +1,10 @@
 /* exported pick */
 function pick(source, keys) {
   var obj = {};
-  var x;
-  for (x in source) {
-    if (source[x] === keys[x]) {
-      obj.push(source[x]);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    if (key in source && source[key] !== undefined) {
+      obj[key] = source[key];
     }
   }
   return obj;
