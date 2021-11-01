cd6ca33982004838cd8690beebf33361d6ce7c09
Nicholas Prodehl <nicholasprodehl@gmail.com>
2021-11-01 13:03:36 -0500

    Finished JavaScript Custom Methods

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,33 @@
+/* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (numbers) {
+    var counter = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      counter = counter + numbers[i];
+    }
+    return counter;
+  },
+  getAverage: function (numbers) {
+    var counter = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      counter = counter + numbers[i];
+    }
+    return counter / numbers.length;
+  }
+};
