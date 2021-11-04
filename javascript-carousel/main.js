50540160d49d78aa1e637373d48b5c5105525059
Nicholas Prodehl <nicholasprodehl@gmail.com>
2021-11-04 12:07:32 -0500

    Not finished yet.

---
+++ javascript-carousel/main.js
@@ -0,0 +1,28 @@
+// var $right = document.querySelector('.fa-chevron-right');
+// var $left = document.querySelector('.fa-chevron-left');
+// var $image = document.querySelector('img');
+// var $circles = document.querySelectorAll('i');
+
+// console.log($right);
+// var counter = 0;
+
+// function handleRight() {
+//   if (counter === 0) {
+//     $image.setAttribute('src', 'images/001.png');
+//   } else if (counter === 1) {
+//     $image.setAttribute('src', 'images/004.png');
+//   } else if (counter === 2) {
+//     $image.setAttribute('src', 'images/007.png');
+//   } else if (counter === 3) {
+//     $image.setAttribute('src', 'images/025.png');
+//   } else if (counter === 4) {
+//     $image.setAttribute('src', 'images/039.png');
+//     counter = 0;
+//   }
+//   counter++;
+// }
+
+// function handleLeft() {
+
+// }
+// $right.addEventListener('click', handleRight);
