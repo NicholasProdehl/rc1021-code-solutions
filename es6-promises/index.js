d3957821b5c0079ad0b7095d5b0b35cd2f86189c
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2021-12-07 20:48:28 +0000

    Finished ES6 Promises

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -1,16 +1,11 @@
 const takeAChance = require('./take-a-chance');
 
-takeAChance('Nicholas');
+const tac = takeAChance('Nicholas');
 
-const promise1 = new Promise((resolve, reject) => {
-  resolve(takeAChance('Nicholas'));
-  reject(takeAChance('Nicholas'));
-});
-
-promise1.then(value => {
+tac.then(value => {
   console.log(value);
 });
 
-promise1.catch(value => {
+tac.catch(value => {
   console.log(value);
 });
