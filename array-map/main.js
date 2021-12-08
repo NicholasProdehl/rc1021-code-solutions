e2f9720f5d4cae73ae736965b6cd4547094e191f
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2021-12-08 23:07:36 +0000

    Finished Array Map

---
+++ array-map/main.js
@@ -0,0 +1,28 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const languages = [
+  'Hypertext Markup Language',
+  'Cascading Style Sheets',
+  'ECMAScript',
+  'PHP Hypertext Preprocessor',
+  'Structured Query Language'
+];
+
+const doubled = numbers.map(x => x * 2);
+
+console.log('Doubled: ', doubled);
+
+const prices = numbers.map(x => {
+  const string = '$' + x + '.00';
+  return string;
+});
+
+console.log('Prices: ', prices);
+
+const upperCased = languages.map(x => x.toUpperCase());
+
+console.log('Upper Cased: ', upperCased);
+
+const firstLetters = languages.map(x => x[0]);
+
+console.log('First Letter: ', firstLetters);
