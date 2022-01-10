d50e1e002c3b98abc062dd7fe1af406ea7ad30d7
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2022-01-10 23:41:00 +0000

    completed big o notation

---
+++ express-notes-api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+const notes = {
+  1: {
+    content: 'The event loop is how a JavaScript runtime pushes asynchronous callbacks onto the stack once the stack is cleared.',
+    id: 1
+  },
+  2: {
+    content: 'Prototypal inheritance is how JavaScript objects delegate behavior.',
+    id: 2
+  },
+  3: {
+    content: 'In JavaScript, the value of `this` is determined when a function is called; not when it is defined.',
+    id: 3
+  },
+  4: {
+    content: 'A closure is formed when a function retains access to variables in its lexical scope.',
+    id: 4
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const notesArray = [];
+  for (const id in notes) {
+    notesArray.push(notes[id]);
+  }
+  res.status(200).json(notesArray);
+});
+
+app.get('/api/grades/:id', (req, res) => {
+  if (req.params.id) {
+    res.sendStatus(400);
+  } else {
+    res.status(200).json(notes[req.params.id]);
+  }
+});
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening to server 3000!');
+});
