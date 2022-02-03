a26f5240117c1d42bbb1d01da34a7960c67a6687
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2022-02-03 22:51:32 +0000

    Not quite done.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,28 +1,84 @@
-// var $right = document.querySelector('.fa-chevron-right');
-// var $left = document.querySelector('.fa-chevron-left');
-// var $image = document.querySelector('img');
-// var $circles = document.querySelectorAll('i');
+var $right = document.querySelector('.fa-chevron-right');
+var $left = document.querySelector('.fa-chevron-left');
+var $image = document.querySelector('img');
+var $circles = document.querySelectorAll('.fa-circle');
 
-// console.log($right);
-// var counter = 0;
+var counter = 0;
 
-// function handleRight() {
-//   if (counter === 0) {
-//     $image.setAttribute('src', 'images/001.png');
-//   } else if (counter === 1) {
-//     $image.setAttribute('src', 'images/004.png');
-//   } else if (counter === 2) {
-//     $image.setAttribute('src', 'images/007.png');
-//   } else if (counter === 3) {
-//     $image.setAttribute('src', 'images/025.png');
-//   } else if (counter === 4) {
-//     $image.setAttribute('src', 'images/039.png');
-//     counter = 0;
-//   }
-//   counter++;
-// }
+setInterval(handleRight, 3000);
+let i = 1;
+function handleRight() {
+  clearInterval(1);
+  clearInterval(i);
+  clearInterval(b);
+  if (counter === 0) {
+    $image.setAttribute('src', 'images/004.png');
+    $circles[0].setAttribute('class', 'far fa-circle one');
+    $circles[1].setAttribute('class', 'fas fa-circle two');
+    counter++;
+  } else if (counter === 1) {
+    $image.setAttribute('src', 'images/007.png');
+    $circles[1].setAttribute('class', 'far fa-circle two');
+    $circles[2].setAttribute('class', 'fas fa-circle three');
+    counter++;
+  } else if (counter === 2) {
+    $image.setAttribute('src', 'images/025.png');
+    $circles[2].setAttribute('class', 'far fa-circle three');
+    $circles[3].setAttribute('class', 'fas fa-circle four');
+    counter++;
+  } else if (counter === 3) {
+    $image.setAttribute('src', 'images/039.png');
+    $circles[3].setAttribute('class', 'far fa-circle four');
+    $circles[4].setAttribute('class', 'fas fa-circle five');
+    counter++;
+  } else if (counter === 4) {
+    $image.setAttribute('src', 'images/001.png');
+    $circles[4].setAttribute('class', 'far fa-circle five');
+    $circles[0].setAttribute('class', 'fas fa-circle one');
+    counter = 0;
+  }
+  // console.log('handleRight counter: ', counter);
+  // console.log('handleRight Selected: ', document.getElementsByClassName('fas fa-circle'));
+  setInterval(handleRight, 3000);
+  i++;
+}
 
-// function handleLeft() {
+let b = 1;
+function handleLeft() {
+  clearInterval(1);
+  clearInterval(b);
+  clearInterval(i);
+  if (counter === 0) {
+    $image.setAttribute('src', 'images/039.png');
+    $circles[2].setAttribute('class', 'far fa-circle three');
+    $circles[3].setAttribute('class', 'fas fa-circle four');
+    counter = 3;
+  } else if (counter === 1) {
+    $image.setAttribute('src', 'images/001.png');
+    $circles[3].setAttribute('class', 'far fa-circle four');
+    $circles[4].setAttribute('class', 'fas fa-circle five');
+    counter = 4;
+  } else if (counter === 2) {
+    $image.setAttribute('src', 'images/004.png');
+    $circles[4].setAttribute('class', 'far fa-circle five');
+    $circles[0].setAttribute('class', 'fas fa-circle one');
+    counter = counter - 2;
+  } else if (counter === 3) {
+    $image.setAttribute('src', 'images/007.png');
+    $circles[0].setAttribute('class', 'far fa-circle one');
+    $circles[1].setAttribute('class', 'fas fa-circle two');
+    counter = counter - 2;
+  } else if (counter === 4) {
+    $image.setAttribute('src', 'images/025.png');
+    $circles[1].setAttribute('class', 'far fa-circle two');
+    $circles[2].setAttribute('class', 'fas fa-circle three');
+    counter = counter - 2;
+  }
+  // console.log('handleLeft Counter:', counter);
+  // console.log('handleLeft Selected: ', document.getElementsByClassName('fas fa-circle'));
+  // handleRight();
+  b++;
+}
 
-// }
-// $right.addEventListener('click', handleRight);
+$right.addEventListener('click', handleRight);
+$left.addEventListener('click', handleLeft);
