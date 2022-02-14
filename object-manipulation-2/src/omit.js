06c830666988f82eec1db6a421eaf5e6bc5bf7f2
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2022-02-14 21:45:44 +0000

    Almost finished Omit

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,7 +1,18 @@
 /* exported omit */
 function omit(source, keys) {
-  var arr = {};
-  for (var i = 0; i < source.length; i++) {
-    arr.sup = 'bro';
+  var obj = source;
+  // console.log('obj: ', obj);
+  for (var i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    // console.log('key: ', key);
+    if (key in source) {
+      delete obj[key];
+    }
   }
+  return obj;
 }
+
+// Create obj output.
+// Search through all keys of source and compare them to the array of keys.
+// If match, do not include in final obj output.
+//
