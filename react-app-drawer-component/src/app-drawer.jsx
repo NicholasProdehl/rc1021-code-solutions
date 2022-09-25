e8f513467636de35399f3c630eab3182819fcab9
Nicholas Prodehl <Nicholasprodehl@gmail.com>
2022-09-25 19:44:09 +0000

    Not finished yet. Added some JSX & CSS

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      modalClass: 'modal hidden',
+      overlayClass: 'overlay'
+    };
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
   }
@@ -11,25 +14,32 @@ class AppDrawer extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="normal page">
-          <i onClick = {this.handleOpen} className="fas fa-bars"></i>
-        </div>
-        <div className="modal">
-          <a href="#" className="menu">Menu</a>
-          <a href="#" className="links">About</a>
-          <a href="#" className="links">Get Started</a>
-          <a href="#" className="links">Sign In</a>
+        <div onClick = {this.handleClose} className = {this.state.overlayClass}>
+          <div className="normal-page">
+            <i onClick = {this.handleOpen} className="fas fa-bars"></i>
+          </div>
+          <div className={this.state.modalClass}>
+            <a href="#" onClick = {this.handleClose} className="links">Menu</a>
+            <a href="#" onClick = {this.handleClose} className="links">About</a>
+            <a href="#" onClick = {this.handleClose} className="links">Get Started</a>
+            <a href="#" onClick = {this.handleClose} className="links">Sign In</a>
+          </div>
         </div>
       </div>
     );
   }
 
   handleOpen() {
-    this.setState({ page: true });
+    this.setState({ modalClass: 'modal' });
+    this.setState({ overlayClass: 'overlay' });
   }
 
   handleClose() {
-    this.setState({ page: false });
+    if (event.target === 'overlay' || event.target === '') {
+      this.setState();
+    }
+    this.setState({ modalClass: 'modal hidden' });
+    this.setState({ overlayClass: 'overlay hidden' });
   }
 }
 
